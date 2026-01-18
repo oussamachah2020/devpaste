44d54ab02ce5cfdb1ffb41a8bbffd853ae46ef45
oussamachah2020 <otaksam213@gmail.com>
2026-01-18 17:58:56 +0100

    backend tests fixed

--- backend/src/pastes/pastes.controller.spec.ts
+++ backend/src/pastes/pastes.controller.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PastesController } from './pastes.controller';
+import { PastesService } from './pastes.service';
 
 describe('PastesController', () => {
   let controller: PastesController;
 
+  const mockPastesService = {
+    create: jest.fn(),
+    findOne: jest.fn(),
+    findAll: jest.fn(),
+    delete: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PastesController],
+      providers: [
+        {
+          provide: PastesService,
+          useValue: mockPastesService,
+        },
+      ],
     }).compile();
 
     controller = module.get<PastesController>(PastesController);
--- backend/src/pastes/pastes.service.spec.ts
+++ backend/src/pastes/pastes.service.spec.ts
@@ -1,12 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PastesService } from './pastes.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 describe('PastesService', () => {
   let service: PastesService;
 
+  const mockPrismaService = {
+    paste: {
+      create: jest.fn(),
+      findUnique: jest.fn(),
+      findMany: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    },
+  };
+
+  const mockCacheManager = {
+    get: jest.fn(),
+    set: jest.fn(),
+    del: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PastesService],
+      providers: [
+        PastesService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+        {
+          provide: CACHE_MANAGER,
+          useValue: mockCacheManager,
+        },
+      ],
     }).compile();
 
     service = module.get<PastesService>(PastesService);
